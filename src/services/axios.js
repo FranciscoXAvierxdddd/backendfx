534ca904f54ba8ea7cdc15489104ef3498aec1b9
FranciscoXAvierxdddd <joseleon666z@gmail.com>
2025-04-03 19:52:31 -0600

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -7,7 +7,7 @@ class Client {
         this.client = axios.create({
             baseURL: 'https://api.cloudflare.com/client/v4',
             headers: {
-                'Authorization': 'Bearer VDBdLLrQDD_f2fvjRxQNEiqAWTxSyzcgrq5GspGq'
+                'Authorization': 'Bearer S04MnbebnKYKLbwhCEbDFcR5KHkpa15q42onOkiZ'
             }
             
         })
